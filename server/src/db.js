5cbdea0493b89eb9c5cec64834250ad5ecc41076
Felipe Santos <felipefss@gmail.com>
2019-03-29 17:40:21 +0000

    Improve db.js

--- server/src/db.js
+++ server/src/db.js
@@ -5,36 +5,59 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'local';
 const client = MongoClient(url, { useNewUrlParser: true });
 
-async function createUser(name) {
+function createUser(name) {
+    const playerData = {
+        name,
+        coins: 1000,
+        inventory: {
+            breads: 30,
+            carrots: 18,
+            diamonds: 1
+        }
+    };
+    return _newEntry('player', playerData);
+}
+
+function getUser(name) {
+    return _get('player', { name });
+}
+
+function updateUser(name, data) {
+    return _updateEntry('player', { name }, data);
+}
+
+/**
+ * 
+ * @param {String} collection - Name of the collection
+ * @param {Object} payload - A JSON object with the data to be inserted
+ */
+async function _newEntry(collection, payload) {
     try {
         await client.connect();
         const db = client.db(dbName);
-        const coll = db.collection('player');
+        const coll = db.collection(collection);
 
-        let r = await coll.insertOne({
-            name,
-            coins: 1000,
-            inventory: {
-                breads: 30,
-                carrots: 18,
-                diamonds: 1
-            }
-        });
+        let r = await coll.insertOne(payload);
         assert.strictEqual(r.insertedCount, 1);
-    } catch(err) {
+    } catch (err) {
         throw err.errmsg;
     } finally {
         client.close();
     }
 }
 
-async function getUser(name) {
+/**
+ * 
+ * @param {String} collection - Name of collection
+ * @param {Object} payload - The object to retrieve
+ */
+async function _get(collection, payload) {
     try {
         await client.connect();
         const db = client.db(dbName);
-        const coll = db.collection('player');
+        const coll = db.collection(collection);
 
-        let user = await coll.findOne({ name });
+        let user = await coll.findOne(payload);
         return user;
     } catch (err) {
         throw err.errmsg;
@@ -43,13 +66,19 @@ async function getUser(name) {
     }
 }
 
-async function updateUser(name, data) {
+/**
+ * 
+ * @param {String} collection - Name of collection
+ * @param {Object} query - The key/value pair of the object to update
+ * @param {Object} payload - JSON ojbect with data to be updated
+ */
+async function _updateEntry(collection, query, payload) {
     try {
         await client.connect();
         const db = client.db(dbName);
-        const coll = db.collection('player');
+        const coll = db.collection(collection);
 
-        let r = await coll.updateOne({ name }, { $set: data });
+        let r = await coll.updateOne(query, { $set: payload });
         assert.strictEqual(r.modifiedCount, 1);
     } catch (err) {
         throw err.errmsg;
