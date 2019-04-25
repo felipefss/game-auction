0b2579fac4ea13509aed2f296efefe5b90c11e0e
Felipe Santos <felipefss@gmail.com>
2019-04-25 14:17:14 +0100

    Finish implementing auction modal.

--- client/js/main/inventory.component.js
+++ client/js/main/inventory.component.js
@@ -1,15 +1,38 @@
 (function () {
     angular.module('game')
-    .component('inventory', {
-        templateUrl: 'js/main/inventory.html',
-        bindings: {
-            items: '<'
-        },
-        controller: InventoryController
-    });
+        .component('inventory', {
+            templateUrl: 'js/main/inventory.html',
+            bindings: {
+                items: '<'
+            },
+            controller: InventoryController
+        });
 
     InventoryController.$inject = [];
-    function InventoryController () {
+    function InventoryController() {
         var $ctrl = this;
+        $ctrl.auctionMinBid = 1;
+
+        $('#confirmAuction').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget);
+            var itemName = button.data('item');
+
+            $ctrl.maxQty = $ctrl.items[itemName];
+            $ctrl.auctionQuantity = $ctrl.maxQty;
+
+            var modal = $(this);
+            modal.find('.modal-title').text('Auction for ' + itemName);
+            modal.find('#quantityInput').val($ctrl.maxQty);
+        });
+
+        $ctrl.startAuction = function () {
+            console.log($ctrl.auctionQuantity);
+        };
+
+        $ctrl.controlLimit = function () {
+            if ($ctrl.auctionQuantity > $ctrl.maxQty) {
+                $ctrl.auctionQuantity = $ctrl.maxQty;
+            }
+        };
     }
 })();
